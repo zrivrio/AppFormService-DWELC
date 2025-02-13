7a8dcb4ab2b836a739b804e8b770997d4e3cba7d
Zahira <zrivrio2103@gmail.com>
2025-02-13 19:53:22 +0100

    ejercicio comentado y con los ultimos requerimientos

--- src/app/components/event-list/event-list.component.ts
+++ src/app/components/event-list/event-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { EventSService } from '../../services/event-s.service';
 import { EventM } from '../../models/eventM.model';
 import { CommonModule } from '@angular/common';
+import { LoggerService } from '../../services/logger.service';
 
 @Component({
   selector: 'app-event-list',
@@ -10,24 +11,51 @@ import { CommonModule } from '@angular/common';
   styleUrl: './event-list.component.css'
 })
 export class EventListComponent implements OnInit {
+
+   //Defeinimos una propiedad que es un array del tipo EventM
+  //En esta propiedada se almacenara la lista de eventos
   eventos: EventM[] = [];
 
-  constructor(private eventService: EventSService) {}
+  //Definimos tres variables que se van a ir incrementando dependiendo del tipo de evento que se añada
+  countLog = 0;
+  countWarn = 0;
+  countError = 0;
 
+  //Aqui se inyecta el eventService como una dependencia
+  constructor(private eventService: EventSService, private loggerService : LoggerService) {}
+
+
+  //Cuando se carge la pagina se llama al evento loadEventos() para cargar los eventos alamcenados en el localStorage al inicializar el servicio
+  //Ademas de que a la array que hemos creado anterior mente le pasamos todos los eventos que hemos creaado anteriormente
   ngOnInit(): void {
     this.eventService.loadEventos();
     this.eventos = this.eventService.getEventos();
+
+    //obtener el conteo de los eventos
+    this.updateCounts();
+
   
-    console.log("Eventos cargados en EventListComponent:", this.eventos);
   }
 
+  //Creamos un metod al cual le vamos a pasar un objeto del tipo EventM
   onFilterChange(event: Event): void {
+    //Obtenemos la selecion que hemos en el select que se encuntra en la vista
     const selectedValue = (event.target as HTMLSelectElement).value;
 
+    //Si la seleccion es all mostramos todos los eventos que hemos creado
+    //Si es distinto filtramos el array por la clasificacion de cada evento
     if (selectedValue === 'all') {
       this.eventos = this.eventService.getEventos();
     } else {
       this.eventos = this.eventService.getEventos().filter(ev => ev.classification === selectedValue);
     }
+  }
+
+  updateCounts() : void {
+    const count = this.loggerService.getCounts();
+
+    this.countLog = count.log;
+    this.countWarn = count.warn;
+    this.countError = count.error;
   }
 }
--- src/app/services/logger.service.ts
+++ src/app/services/logger.service.ts
@@ -1,18 +1,33 @@
 import {Injectable} from '@angular/core';
-import {count} from "rxjs";
+import { EventM } from '../models/eventM.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoggerService {
 
+  //Definir tres propediades que llevaran el conteo de cuantas veces se ha llama a cada tipo de log
   countLog =0;
   countWarn =0;
   countError =0;
 
   constructor() {
+    this.loadCountsFormStorage();
   }
 
+  //Obtener conteos desde el localStorage
+  private loadCountsFormStorage(): void {
+    const eventosGuardados = localStorage.getItem('eventos');
+    if (eventosGuardados){
+      const eventos : EventM[] = JSON.parse(eventosGuardados);
+      this.countLog = eventos.filter(event => event.classification === 'log').length;
+      this.countWarn = eventos.filter(event => event.classification === 'warn').length;
+      this.countError = eventos.filter(event => event.classification === 'error').length;
+    }
+  }
+
+  //Merodo que devuelve un objeto con los conteos actuales de logs, warnings y errors
+  //Este metodo pue eser llmado en cualquier componente o srvicio que necesite conocer el estado actual de los conteos
   getCounts() {
     return {
       log: this.countLog,
@@ -21,9 +36,20 @@ export class LoggerService {
     };
   }
 
+  //Metodo quee actualiza el conteo de los logs, warnings y de los errors
+  //Recibe le parametro calisificacion que puede ser log, warn o error
+  //Dependiedo del valor de esta clasificacion se incrementara el contador correpondiente
    updateCounts(classification: 'log' | 'warn' | 'error') {
     if (classification === 'log') this.countLog++;
     else if (classification === 'warn') this.countWarn++;
     else if (classification === 'error') this.countError++;
+  
+    this.saveCountsToStorage();
+  
   }
+  
+  private saveCountsToStorage() :void{
+    localStorage.setItem('counts', JSON.stringify(this.getCounts()));
+  }
+
 }
