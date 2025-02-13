88ccacad0a4d60f307abfe1dd09056e11484b8c3
Zahira <zrivrio2103@gmail.com>
2025-02-13 20:18:46 +0100

    Update logger.service.ts

--- src/app/services/logger.service.ts
+++ src/app/services/logger.service.ts
@@ -5,8 +5,6 @@ import { EventM } from '../models/eventM.model';
   providedIn: 'root'
 })
 export class LoggerService {
-
-  //Definir tres propediades que llevaran el conteo de cuantas veces se ha llama a cada tipo de log
   countLog =0;
   countWarn =0;
   countError =0;
@@ -15,7 +13,6 @@ export class LoggerService {
     this.loadCountsFormStorage();
   }
 
-  //Obtener conteos desde el localStorage
   private loadCountsFormStorage(): void {
     const eventosGuardados = localStorage.getItem('eventos');
     if (eventosGuardados){
@@ -26,8 +23,6 @@ export class LoggerService {
     }
   }
 
-  //Merodo que devuelve un objeto con los conteos actuales de logs, warnings y errors
-  //Este metodo pue eser llmado en cualquier componente o srvicio que necesite conocer el estado actual de los conteos
   getCounts() {
     return {
       log: this.countLog,
@@ -36,9 +31,6 @@ export class LoggerService {
     };
   }
 
-  //Metodo quee actualiza el conteo de los logs, warnings y de los errors
-  //Recibe le parametro calisificacion que puede ser log, warn o error
-  //Dependiedo del valor de esta clasificacion se incrementara el contador correpondiente
    updateCounts(classification: 'log' | 'warn' | 'error') {
     if (classification === 'log') this.countLog++;
     else if (classification === 'warn') this.countWarn++;
