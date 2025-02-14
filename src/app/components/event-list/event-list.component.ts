afb556cb930fcdf5f720563587bcce1b9c4e766b
zrivrio <zrivrio2103@gmail.com>
2025-02-14 10:48:07 +0100

    modificaciones examen

--- src/app/components/event-list/event-list.component.ts
+++ src/app/components/event-list/event-list.component.ts
@@ -12,26 +12,38 @@ import { LoggerService } from '../../services/logger.service';
 })
 export class EventListComponent implements OnInit {
 
+   //Defeinimos una propiedad que es un array del tipo EventM
+  //En esta propiedada se almacenara la lista de eventos
   eventos: EventM[] = [];
 
+  //Definimos tres variables que se van a ir incrementando dependiendo del tipo de evento que se añada
   countLog = 0;
   countWarn = 0;
   countError = 0;
 
+  //Aqui se inyecta el eventService como una dependencia
   constructor(private eventService: EventSService, private loggerService : LoggerService) {}
 
+
+  //Cuando se carge la pagina se llama al evento loadEventos() para cargar los eventos alamcenados en el localStorage al inicializar el servicio
+  //Ademas de que a la array que hemos creado anterior mente le pasamos todos los eventos que hemos creaado anteriormente
   ngOnInit(): void {
     this.eventService.loadEventos();
     this.eventos = this.eventService.getEventos();
 
+    //obtener el conteo de los eventos
     this.updateCounts();
 
   
   }
 
+  //Creamos un metod al cual le vamos a pasar un objeto del tipo EventM
   onFilterChange(event: Event): void {
+    //Obtenemos la selecion que hemos en el select que se encuntra en la vista
     const selectedValue = (event.target as HTMLSelectElement).value;
 
+    //Si la seleccion es all mostramos todos los eventos que hemos creado
+    //Si es distinto filtramos el array por la clasificacion de cada evento
     if (selectedValue === 'all') {
       this.eventos = this.eventService.getEventos();
     } else {
--- src/app/services/employee-s.service.ts
+++ src/app/services/employee-s.service.ts
@@ -7,24 +7,40 @@ import { EmployeeM } from '../models/employeeM.model';
 })
 export class EmployeeSService {
 
+  //Defino un BehaviorSubject para mantener y emitir l lista de empleados 
+  //Inicalmente su valor esta vacio
   private employeesSubject = new BehaviorSubject<EmployeeM[]>([]); 
   
+  //Defino un BehaviorSubject para mantener y emitir el emplado selecionado 
+  //Inicialmente el valor es null
   private selectedEmployeeSubject = new BehaviorSubject<EmployeeM | null>(null);
+
+  //Tambien se llama al evento loadEventos() para cargar los eventos alamcenados en el localStorage al inicializar el servicio
   constructor() {
     this.loadEmployees();
   }
 
+  // Método para obtener un Observable de la lista de empleados.
+  // Los componentes pueden suscribirse a este Observable para recibir actualizaciones cuando la lista de empleados cambie.
   getEmployees() {
     return this.employeesSubject.asObservable(); 
   }
 
+// Método para obtener un Observable del empleado seleccionado.
+  // Los componentes pueden suscribirse a este Observable para recibir actualizaciones cuando el empleado seleccionado cambie.
   getSelectedEmployee() {
     return this.selectedEmployeeSubject.asObservable();
   }
 
+ // Método para establecer el empleado seleccionado.
+  // Recibe un parámetro 'employee' que puede ser un objeto EmployeeM o null.
   setSelectedEmployee(employee: EmployeeM | null) {
+
+    // Actualiza el valor del BehaviorSubject 'selectedEmployeeSubject' con el empleado proporcionado.
     this.selectedEmployeeSubject.next(employee); 
     
+    // Si el empleado no es null, se guarda en el localStorage bajo la clave 'selectedEmployee'.
+    // Si el empleado es null, se elimina la clave 'selectedEmployee' del localStorage.
     if (employee) {
       localStorage.setItem('selectedEmployee', JSON.stringify(employee));
     } else {
@@ -32,12 +48,16 @@ export class EmployeeSService {
     }
   }
 
+  //Metodo para cargar la lista de empleados desde el servidor json
   private loadEmployees() {
+    //Utiliza la direcion de donde esta ubicado el servidor json
     fetch('http://localhost:3000/employee')
-      .then(response => response.json()) 
+      .then(response => response.json()) //Convierte la s respuesta a json
       .then(data => {
+          // Actualiza el valor del BehaviorSubject 'employeesSubject' con los datos recibidos.
+          //El next es el quesirve para pasarle los datos a los componentes que esta suscritos a este Observable
         this.employeesSubject.next(data); 
       })
-      .catch(error => console.error('Error al cargar los empleados:', error));
+      .catch(error => console.error('Error al cargar los empleados:', error)); //Maneja los errores que puedan ocurrir
   }
 }
